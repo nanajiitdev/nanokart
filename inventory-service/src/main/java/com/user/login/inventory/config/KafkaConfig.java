b25294bc893368d03d6498f9e377d504e5bc54d2
Nanaji Ruttala <nanaji.itdev@gmail.com>
2026-08-19 18:17:17 +0530

    Update Kafka configuration for inventory and payment

--- inventory-service/src/main/java/com/user/login/inventory/config/KafkaConfig.java
+++ inventory-service/src/main/java/com/user/login/inventory/config/KafkaConfig.java
@@ -1,20 +1,9 @@
 package com.user.login.inventory.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import com.user.login.inventory.event.InventoryUpdatedEvent;
 
 @Configuration
 public class KafkaConfig {
@@ -28,38 +17,30 @@ public class KafkaConfig {
                 .build();
     }
 
-    @Bean
-    public ProducerFactory<String, InventoryUpdatedEvent> producerFactory() {
-
-        Map<String, Object> config = new HashMap<>();
-
-        config.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092"
-        );
-
-        config.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class
-        );
-
-        config.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class
-        );
-
-        return new DefaultKafkaProducerFactory<>(config);
-    }
-
-    @Bean
-    public KafkaTemplate<String, InventoryUpdatedEvent> kafkaTemplate() {
-
-        KafkaTemplate<String, InventoryUpdatedEvent> template =
-                new KafkaTemplate<>(producerFactory());
-
-        // Enable Micrometer tracing for Kafka producer
-        template.setObservationEnabled(true);
-
-        return template;
-    }
+	/*
+	 * @Bean public ProducerFactory<String, InventoryUpdatedEvent> producerFactory()
+	 * {
+	 * 
+	 * Map<String, Object> config = new HashMap<>();
+	 * 
+	 * config.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092" );
+	 * 
+	 * config.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+	 * StringSerializer.class );
+	 * 
+	 * config.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+	 * JsonSerializer.class );
+	 * 
+	 * return new DefaultKafkaProducerFactory<>(config); }
+	 * 
+	 * @Bean public KafkaTemplate<String, InventoryUpdatedEvent> kafkaTemplate() {
+	 * 
+	 * KafkaTemplate<String, InventoryUpdatedEvent> template = new
+	 * KafkaTemplate<>(producerFactory());
+	 * 
+	 * // Enable Micrometer tracing for Kafka producer
+	 * template.setObservationEnabled(true);
+	 * 
+	 * return template; }
+	 */
 }
--- payment-service/src/main/java/com/ecommerce/paymentservice/config/KafkaConfig.java
+++ payment-service/src/main/java/com/ecommerce/paymentservice/config/KafkaConfig.java
@@ -1,20 +1,9 @@
 package com.ecommerce.paymentservice.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import com.ecommerce.paymentservice.event.PaymentCompletedEvent;
 
 @Configuration
 public class KafkaConfig {
@@ -30,32 +19,29 @@ public class KafkaConfig {
 
     }
 
-    @Bean
-    public ProducerFactory<String, PaymentCompletedEvent> producerFactory() {
-
-        Map<String, Object> config = new HashMap<>();
-
-        config.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-
-        config.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-
-        config.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(config);
-
-    }
-
-    @Bean
-    public KafkaTemplate<String, PaymentCompletedEvent> kafkaTemplate() {
-
-        return new KafkaTemplate<>(producerFactory());
-
-    }
+	/*  commented out because we are using Spring Boot's auto-configuration for Kafka producer for docker
+	 * @Bean public ProducerFactory<String, PaymentCompletedEvent> producerFactory()
+	 * {
+	 * 
+	 * Map<String, Object> config = new HashMap<>();
+	 * 
+	 * config.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	 * 
+	 * config.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+	 * StringSerializer.class);
+	 * 
+	 * config.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+	 * JsonSerializer.class);
+	 * 
+	 * return new DefaultKafkaProducerFactory<>(config);
+	 * 
+	 * }
+	 * 
+	 * @Bean public KafkaTemplate<String, PaymentCompletedEvent> kafkaTemplate() {
+	 * 
+	 * return new KafkaTemplate<>(producerFactory());
+	 * 
+	 * }
+	 */
 
 }
